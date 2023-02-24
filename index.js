8ab01ac05a65f3e274374d370ec2946009df0dde
delowar-Hosen <delowarhosen15o1121@gmail.com>
2023-02-25 00:31:29 +0600

    let try

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+require("dotenv").config();
 const exppress = require("express");
 const chalk = require("chalk");
+const routes = require("./routes");
 const app = exppress();
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
+//middlware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
 
 app.listen("3000", (req, res) => {
   console.log(chalk.blue.bgRed.bold("Port on running 3000"));
