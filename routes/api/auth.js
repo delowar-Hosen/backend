5b12e48425ba57412d81fd98e6eab6401d3f2124
delowar-Hosen <delowarhosen15o1121@gmail.com>
2023-02-25 22:58:12 +0600

    schema

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,20 +1,32 @@
 const express = require("express");
 const _ = express.Router();
+const User = require("../../models/user.js");
 
 _.post("/registration", (req, res) => {
-  const { email, password, fullname, isSubscribed } = req.body;
+  const { email, phoneNumber, firstName, lastName, password } = req.body;
 
   if (!email) {
     return res.json({ error: "You Must Give An Email" });
   }
-  if (!fullname) {
-    return res.json({ error: "You Must Give A Fullname" });
+  if (!firstName) {
+    return res.json({ error: "You Must Give A Firstname" });
+  }
+  if (!lastName) {
+    return res.json({ error: "You Must Give A Lastname" });
   }
   if (!password) {
     return res.json({ error: "You Must Give A Password" });
   }
 
-  res.json({ email, password, fullname, isSubscribed });
+  const user = new User({
+    email,
+    firstName,
+    lastName,
+    password,
+  });
+
+  user.save();
+  res.json(user);
 });
 
 module.exports = _;
