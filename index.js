845cd97adefe916ae78d55a76cc1f40f490c57b3
delowar-Hosen <delowarhosen15o1121@gmail.com>
2023-02-25 15:58:45 +0600

    adddb

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const chalk = require("chalk");
 const routes = require("./routes");
+const setUpDb = require("./config/dbConfig");
 var cors = require("cors");
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 app.use(routes);
+setUpDb();
 
 app.listen("3000", (req, res) => {
   console.log(chalk.blue.bgRed.bold("Port on running 3000"));
