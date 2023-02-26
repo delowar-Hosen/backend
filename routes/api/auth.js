8f3a4b02cbc4c12c0576a7c46e0efbcaf6b6f891
delowar-Hosen <delowarhosen15o1121@gmail.com>
2023-02-26 17:45:45 +0600

    let

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,34 +1,40 @@
 const express = require("express");
 const _ = express.Router();
 const User = require("../../models/user.js");
+//QjfvnZJD7OihaJny
+
+//backend-test
 
 _.post("/registration", (req, res) => {
-  const { email, phoneNumber, firstName, lastName, password } = req.body;
+  try {
+    const { email, phoneNumber, firstName, lastName, password } = req.body;
 
-  if (!email) {
-    return res.json({ error: "You Must Give An Email" });
-  }
-  if (!firstName) {
-    return res.json({ error: "You Must Give A Firstname" });
-  }
-  if (!lastName) {
-    return res.json({ error: "You Must Give A Lastname" });
-  }
-  if (!password) {
-    return res.json({ error: "You Must Give A Password" });
-  }
+    if (!email) {
+      return res.json({ error: "You Must Give An Email" });
+    }
+    if (!firstName) {
+      return res.json({ error: "You Must Give A Firstname" });
+    }
+    if (!lastName) {
+      return res.json({ error: "You Must Give A Lastname" });
+    }
+    if (!password) {
+      return res.json({ error: "You Must Give A Password" });
+    }
 
-  const user = new User({
-    email,
-    phoneNumber,
-    firstName,
-    lastName,
-    password,
-  });
+    const user = new User({
+      email,
+      phoneNumber,
+      firstName,
+      lastName,
+      password,
+    });
 
-  user.save();
-
-  res.json(user);
+    user.save();
+    res.json(user);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = _;
