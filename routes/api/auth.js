8d244935079824b72d12a0796c46d95cc1c93586
delowar-Hosen <delowarhosen15o1121@gmail.com>
2023-02-26 22:53:32 +0600

    k

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,46 +5,42 @@ const jwt = require("jsonwebtoken");
 const { sendVerificationEmail } = require("../../helpers/emailSender.js");
 
 _.post("/registration", async (req, res) => {
-  try {
-    const { email, phoneNumber, firstName, lastName, password } = req.body;
+  const { email, phoneNumber, firstName, lastName, password } = req.body;
 
-    if (!email) {
-      return res.json({ error: "You Must Give An Email" });
-    }
-    if (!firstName) {
-      return res.json({ error: "You Must Give A Firstname" });
-    }
-    if (!lastName) {
-      return res.json({ error: "You Must Give A Lastname" });
-    }
-    if (!password) {
-      return res.json({ error: "You Must Give A Password" });
-    }
-
-    const user = new User({
-      email,
-      phoneNumber,
-      firstName,
-      lastName,
-      password,
-    });
-
-    user.save();
-
-    let username = user.firstName + user.lastName;
-
-    let token = jwt.sign(
-      { email: user.email },
-      "gfgdgsfdsdfvewafdfghjkmdt156415",
-      { expiresIn: "30m" }
-    );
-
-    sendVerificationEmail(user.email, username, token);
-
-    res.json(user);
-  } catch (error) {
-    console.log(error);
+  if (!email) {
+    return res.json({ error: "You Must Give An Email" });
   }
+  if (!firstName) {
+    return res.json({ error: "You Must Give A Firstname" });
+  }
+  if (!lastName) {
+    return res.json({ error: "You Must Give A Lastname" });
+  }
+  if (!password) {
+    return res.json({ error: "You Must Give A Password" });
+  }
+
+  const user = new User({
+    email,
+    phoneNumber,
+    firstName,
+    lastName,
+    password,
+  });
+
+  user.save();
+
+  let username = user.firstName + user.lastName;
+
+  let token = jwt.sign(
+    { email: user.email },
+    "gfgdgsfdsdfvewafdfghjkmdt156415",
+    { expiresIn: "30m" }
+  );
+
+  sendVerificationEmail(user.email, username, token);
+
+  res.json(user);
 });
 
 module.exports = _;
